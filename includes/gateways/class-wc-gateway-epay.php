0b098886a8191802e644397a9f5f8471d8ba4fe3
创意电子 <wxcydz@gmail.com>
2026-08-19 02:56:49 +0800

    Add payment method icons

--- includes/gateways/class-wc-gateway-epay.php
+++ includes/gateways/class-wc-gateway-epay.php
@@ -22,40 +22,47 @@ class EPAY_Gateway_Alipay extends EPAY_Abstract_Gateway {
 class EPAY_Gateway_Bank extends EPAY_Abstract_Gateway {
 	protected $epay_type  = 'bank';
 	protected $epay_title = '银联支付';
+	protected $epay_icon  = 'bank.svg';
 }
 
 class EPAY_Gateway_Revolut extends EPAY_Abstract_Gateway {
 	protected $epay_type  = 'revolut';
 	protected $epay_title = 'Revolut';
+	protected $epay_icon  = 'revolut.svg';
 }
 
 class EPAY_Gateway_Paypal extends EPAY_Abstract_Gateway {
 	protected $epay_type  = 'paypal';
 	protected $epay_title = 'PayPal';
+	protected $epay_icon  = 'paypal.svg';
 }
 
 class EPAY_Gateway_Alipayhk extends EPAY_Abstract_Gateway {
 	protected $epay_type  = 'alipayhk';
 	protected $epay_title = 'AlipayHK';
-	protected $epay_icon  = 'zfb-logo.jpg';
+	protected $epay_icon  = 'alipayhk.svg';
 }
 
 class EPAY_Gateway_Usdt extends EPAY_Abstract_Gateway {
 	protected $epay_type  = 'usdt';
 	protected $epay_title = 'USDT';
+	protected $epay_icon  = 'usdt.svg';
 }
 
 class EPAY_Gateway_Linepay extends EPAY_Abstract_Gateway {
 	protected $epay_type  = 'linepay';
 	protected $epay_title = 'LINE Pay';
+	protected $epay_icon  = 'linepay.svg';
 }
 
 class EPAY_Gateway_Paynow extends EPAY_Abstract_Gateway {
 	protected $epay_type  = 'paynow';
 	protected $epay_title = 'PayNow';
+	protected $epay_icon  = 'paynow.svg';
 }
 
 class EPAY_Gateway_Card extends EPAY_Abstract_Gateway {
 	protected $epay_type  = 'card';
 	protected $epay_title = '信用卡 / 借记卡';
+	protected $epay_icon  = 'card.svg';
 }
